5d2626fb87614ca29fd3385d829e6de3521ff164
DevHumbleChris <dev.humblechris254@gmail.com>
2023-03-06 10:13:33 +0300

    added auth routes

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,22 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Get } from '@nestjs/common';
 import { AuthService } from './auth.service';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
+
+  @Post('signup')
+  async signup() {
+    return 'Signup route';
+  }
+
+  @Post('signin')
+  async signin() {
+    return 'Signin route';
+  }
+
+  @Get('signout')
+  async Signout() {
+    return 'Signout route';
+  }
 }
